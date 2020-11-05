66914b93b0915d29fd3353e22d5042c345d2d90b
Tari <akinn.yinka@gmail.com>
2020-11-05 22:26:30 +0100

    Update band_name_generator.py
    used the 'f' string in place of concatenation

--- band_name_generator/band_name_generator.py
+++ band_name_generator/band_name_generator.py
@@ -1,4 +1,4 @@
 print("Hello, welcome. Let's find you a band name\n")
 city = input("What city did you grow up in?\n")
-pet_name = ("What is your pet's name?\n")
-print("Your band name is " +city+ " " +pet_name+ "!")
+pet_name = input("What is your pet's name?\n")
+print(f"Your band name is {city} {pet_name}!")
